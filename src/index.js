bb7b64a5ecd2480e42002ca914b7551114b5a831
Andrea <avillagomez1@miners.utep.edu>
2024-10-27 09:10:07 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlYGNC5wrcBciC2ji5tuM7cA_lFcHiKpI",
+  authDomain: "uchat-be475.firebaseapp.com",
+  projectId: "uchat-be475",
+  storageBucket: "uchat-be475.appspot.com",
+  messagingSenderId: "943842421013",
+  appId: "1:943842421013:web:c78560f2e11b4d3f53211c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
