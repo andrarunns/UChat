0265c2f415f508cf17acb328b0afe5aa04fcee9a
Andrea <avillagomez1@miners.utep.edu>
2024-10-28 13:02:13 -0600

    Create account and store in database

--- src/index.js
+++ src/index.js
@@ -1,17 +1,119 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { browserSessionPersistence, createUserWithEmailAndPassword, getAuth, setPersistence, signOut, updateProfile } from "firebase/auth";
+import { getFirestore, collection, doc, setDoc } from "firebase/firestore";
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDlYGNC5wrcBciC2ji5tuM7cA_lFcHiKpI",
-  authDomain: "uchat-be475.firebaseapp.com",
-  projectId: "uchat-be475",
-  storageBucket: "uchat-be475.appspot.com",
-  messagingSenderId: "943842421013",
-  appId: "1:943842421013:web:c78560f2e11b4d3f53211c"
+  apiKey: "AIzaSyBgkWWt1NZL_QWAhsUyxAuF91i5V0P2JHc",
+  authDomain: "uchat-d9392.firebaseapp.com",
+  projectId: "uchat-d9392",
+  storageBucket: "uchat-d9392.appspot.com",
+  messagingSenderId: "397312451749",
+  appId: "1:397312451749:web:6128f84f311b045f9d194c"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore and Auth
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+console.log("Firestore initialized:", db);
+console.log("Auth initialized:", auth);
+
+// Create a reference variable to the collection
+const userCol = collection(db, "users");
+
+// Adding a user to the "users" collection only
+const addAccountForm = document.querySelector("#createAccount");
+
+if (addAccountForm) {
+  addAccountForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const firstname = addAccountForm.firstName.value;
+    const lastname = addAccountForm.lastName.value;
+    const email = addAccountForm.email.value;
+    const pass = addAccountForm.password.value;
+
+    // Set session persistence
+    setPersistence(auth, browserSessionPersistence)
+      .then(() => {
+        // Create the user with email and password
+        return createUserWithEmailAndPassword(auth, email, pass);
+      })
+      .then((userCredential) => {
+        const user = userCredential.user;
+        const uid = user.uid;
+
+        // Reference to the new user document
+        const userRef = doc(db, "users", uid);
+
+        // New user data to add
+        const newUser = {
+          firstname: firstname,
+          lastname: lastname,
+          email: email,
+          uid: uid,
+          friends: {} // or any other fields you want to initialize
+        };
+
+        // Set new user data in Firestore
+        return setDoc(userRef, newUser);
+      })
+      .then(() => {
+        console.log("New user added successfully");
+        
+        // Redirect to dashboard or other page if needed
+        location.href = "messagingDashboard.html";
+      })
+      .catch((error) => {
+        console.log("Error creating user or adding to Firestore:", error);
+      });
+  });
+}
+
+
+
+// Log in Form ------------------------------------------------------------------
+// const loginForm = document.getElementById("login")
+// if (loginForm) {
+//   loginForm.addEventListener("submit", (event) => {
+//     event.preventDefault()
+//     const wrongLogInMessage = document.getElementById('wrongLogIn');
+//     setPersistence(auth, browserSessionPersistence)
+//       .then(() => {
+//         const email = loginForm.email.value
+//         const pass = loginForm.password.value
+//         console.log(email)
+//         console.log(pass)
+//         signInWithEmailAndPassword(auth, email, pass)
+//           .then((userCredential) => {
+//             console.log("Signed In with Created User")
+//             console.log(userCredential.user.uid)
+//             var userRef = doc(userCol, userCredential.user.uid)
+//             console.log(userRef)
+//             getDoc(userRef)
+//               .then((userSnap) => {
+//                 console.log(userSnap.data())
+//                 if (userSnap.data().type == 'admin') {
+//                   location.href = "admin_dash.html"
+//                 } else {
+//                   location.href = "dashboard.html"
+//                 }
+//               }).catch((e) => {
+//                 console.log(e)
+//               })
+//           }).catch((e) => {
+//             console.log(e)
+//             wrongLogInMessage.style.display = 'block';
+//           })
+//       }).catch((e) => {
+//         console.log("Persistence error 2")
+//       })
+//   })
+// }
+
